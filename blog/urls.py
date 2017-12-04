c43b66aac36adb945bf7e934b52b99db3e3f9e44
rbed <radekbednarz@gmail.com>
2017-12-05 00:00:41 +0100

    dodanie widoku, urla i templetki dla poj wpisu

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from .views import article_list
+from .views import article_list, article_detail
 
 
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', article_list, name='post-list'),
-#    url(r'^(?P<pk>[0-9]+)/$', customers_detail, name='detail1'),
+    url(r'^(?P<pk>[0-9]+)/$', article_detail, name='art-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 from django.utils import timezone
 
@@ -11,4 +11,8 @@ from django.utils import timezone
 def article_list(request):
     article = Article.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
     return render(request, 'blog/post_list.html', {'article': article})
+
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, 'blog/art_detail.html', {'art': article})
 
